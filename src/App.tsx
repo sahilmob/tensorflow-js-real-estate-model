737c19848d230ae91ded8e5b3704e815991d26c6
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-04-27 19:45:16 +0300

    model creation and training

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ tf.util.shuffleCombo(INPUTS, OUTPUTS);
 const INPUT_TENSOR = tf.tensor2d(INPUTS);
 const OUTPUT_TENSOR = tf.tensor1d(OUTPUTS);
 
-function normalize(tensor: tf.Tensor2D, min?: tf.Tensor1D, max?: tf.Tensor1D) {
+function normalize(tensor: tf.Tensor2D, min?: tf.Tensor, max?: tf.Tensor) {
   const result = tf.tidy(() => {
     const MIN_VALUES = min || tf.min(tensor, 0);
     const MAX_VALUES = max || tf.max(tensor, 0);
@@ -38,6 +38,66 @@ FEATURE_RESULT.MIN_VALUES.print();
 FEATURE_RESULT.MAX_VALUES.print();
 
 INPUT_TENSOR.dispose();
+
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 1, inputShape: [2] }));
+model.summary();
+
+async function train() {
+  const LEARNING_RATE = 0.01;
+
+  model.compile({
+    optimizer: tf.train.sgd(LEARNING_RATE),
+    loss: tf.losses.meanSquaredError,
+  });
+
+  const result = await model.fit(
+    FEATURE_RESULT.NORMALIZED_VALUES,
+    OUTPUT_TENSOR,
+    {
+      validationSplit: 0.15,
+      shuffle: true,
+      batchSize: 64,
+      epochs: 10,
+    }
+  );
+
+  OUTPUT_TENSOR.dispose();
+  FEATURE_RESULT.NORMALIZED_VALUES.dispose();
+
+  console.log(
+    "Average error loss:" +
+      Math.sqrt(result.history.loss[result.history.loss.length - 1] as number)
+  );
+  console.log(
+    "Average validation loss:" +
+      Math.sqrt(
+        result.history.val_loss[result.history.val_loss.length - 1] as number
+      )
+  );
+
+  evaluate();
+}
+
+train();
+
+function evaluate() {
+  tf.tidy(() => {
+    const newInput = normalize(
+      tf.tensor2d([[750, 1]]),
+      FEATURE_RESULT.MIN_VALUES,
+      FEATURE_RESULT.MAX_VALUES
+    );
+    const output = model.predict(newInput.NORMALIZED_VALUES) as tf.Tensor;
+    output.print();
+  });
+
+  FEATURE_RESULT.MIN_VALUES.dispose();
+  FEATURE_RESULT.MAX_VALUES.dispose();
+  model.dispose();
+
+  console.log(tf.memory().numTensors);
+}
 
 function App() {
   return <></>;
